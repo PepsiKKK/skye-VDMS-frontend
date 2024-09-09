f87a452258761d6c10447c543577cedba481355f
skye <1652454160@qq.com>
2024-09-09 17:39:36 +0800

    表格生成相关代码

--- config/routes.ts
+++ config/routes.ts
@@ -4,6 +4,9 @@ export default [
       { name:'注册',path: '/user/register', component: './User/Register' },
     ] },
   { name:'欢迎页面',path: '/welcome', icon: 'smile', component: './Welcome' },
+  {path: '/', redirect: '/add_chart' },
+  {name: '智能分析', path: 'add_chart', icon: 'barChart', component: './AddChart' },
+
   {
     path: '/admin',
     icon: 'crown',
@@ -14,7 +17,7 @@ export default [
       { path: '/admin/sub-page', component: './Admin' },
     ],
   },
-  { icon: 'table', path: '/list', component: './TableList', name:'表格页',},
+  // { icon: 'table', path: '/list', component: './TableList', name:'表格页',},
   { path: '/', redirect: '/welcome' },
   { path: '*', layout: false, component: './404' },
 ];
